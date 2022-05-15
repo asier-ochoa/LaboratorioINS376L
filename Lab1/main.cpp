1082196ade43f7e40e46c5a8cc84eb9155256e0b
bletinor <yjtv@protonmail.com>
2022-05-15 16:17:25 -0400

    wrote name and id

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,5 +1,5 @@
 //ENUNCIADO: Hacer juego de mastermind
-//PARTICIPANTES: 1101331 Asier Ochoa
+//PARTICIPANTES: 1101331 Asier Ochoa, 1099399 Yeuris Terrero
 //FECHA: 4/15/22
 #include <iostream>
 #include <vector>
