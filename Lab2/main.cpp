f27996ea5a7608a26ac1178e46340229be1f2dec
asier-ochoa <asier-ochoa>
2022-05-18 23:31:04 -0400

    Random number generation working

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 #include <cstdlib>
-
+#include <cstring>
 
 class guest{ // DON'T USE VECTORS, BOY
     private:
@@ -13,12 +13,39 @@ class guest{ // DON'T USE VECTORS, BOY
         size_t size;
         size_t elements;
 
+        void expand();
+
     public:
-        guest(): num(), list(new unsigned int[1]), size(1), elements(0) {};
+        guest(): num(rand() % 99 + 1), list(new unsigned int[1]), size(1), elements(0) {list[0] = num;}
+        guest(const guest& other)
+                : num(other.num), list(new unsigned int[other.size]), size(other.size), elements(other.elements)
+            {memcpy(list, other.list, size);}
         ~guest() {delete[] list;}
+
+        void makeList();
+        unsigned int& getNum() {return num;}
 };
 
+void guest::expand() {
+    auto* tmp = new unsigned int[size * 2];
+    memcpy(tmp, list, size);
+    size *= 2;
+    delete[] list;
+    list = tmp;
+}
+
+void guest::makeList() {
+    while (num != 4){
+
+    }
+}
+
 int main(){
-    std::cout << "HEY\n";
+    srand(time(nullptr)); //Seed with unix time
+    guest party[99](10); //calls constructor on every member (BRUH)
+    for (int i = 0; i < 99; ++i) {
+        std::cout << party[i].getNum() << ';';
+    }
+    std::cout << '\n';
     return 0;
 }
