7eca07d5d604a082d0ea7bf691e27078aa7ed13b
asier-ochoa <asier-ochoa>
2022-05-18 21:10:32 -0400

    Started class for guest

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -3,6 +3,20 @@
 //FECHA:
 
 #include <iostream>
+#include <cstdlib>
+
+
+class guest{ // DON'T USE VECTORS, BOY
+    private:
+        unsigned int num;
+        unsigned int* list;
+        size_t size;
+        size_t elements;
+
+    public:
+        guest(): num(), list(new unsigned int[1]), size(1), elements(0) {};
+        ~guest() {delete[] list;}
+};
 
 int main(){
     std::cout << "HEY\n";
