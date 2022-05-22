4d6966a99016cb9d50ba599fb503aa1cf3a09314
bletinor <yjtv@protonmail.com>
2022-05-22 15:03:10 -0400

    completed algo. Finds longest chain and number that prodcues it

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -3,8 +3,49 @@
 //FECHA:
 
 #include <iostream>
+#include <algorithm>
 
 int main(){
-    std::cout << "HEY\n";
+
+    int num, chain[99] = {0}, num_result;
+
+    for (int i = 1; i < 99; i++) //checks all numbers between 1-99
+    {
+        num = i; //uses the iterator number withot affecting th loop
+
+        do
+        {
+            if ((num%2) == 0)
+            {
+                num = num/2; //if number is even, divide by 2
+            }
+            else
+            {
+                num = (num * 3) + 1; //if number is odd, multiply by 3 and add 1
+            }
+
+            chain[i]++; //counts how many operations have been done to the number until the result is 4
+        } while (num != 4);
+        
+    }
+    
+    /*for (size_t j = 0; j < 99; j++)
+    {
+       std::cout << j << " " << chain[j] << "\n"; //prints all chains, useless xd
+    }*/
+
+    for (size_t i = 0; i < 99; i++) //finds longest chain
+    {
+        if (chain[0] < chain[i])
+        {
+            chain[0] = chain[i]; //finds de VALUE of the longest chain
+            num_result = i; //stores the NUMBER that produces the longest chain
+        }
+    }
+
+    std::cout << "Longest chain: " << chain[0] << "\n" << "Number: " << num_result; //prints longest chaing and number that produces it
+    
+
+
     return 0;
 }
