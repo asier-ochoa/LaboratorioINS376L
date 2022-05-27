b0a8c00453a7731cc05f52fbd722b3a363eee491
asier-ochoa <asier-ochoa>
2022-05-27 01:26:34 -0400

    Added priority mecanism

--- Lab3/queue.cpp
+++ Lab3/queue.cpp
@@ -3,14 +3,17 @@
 //FECHA:
 
 #include <iostream>
+#include <utility>
 
 template<typename T>
+//REMEMBER!!! first-> node-> last-> NULL
 class Queue {
     private:
         struct Node {
-            Node(T val): value(val), next(nullptr){}
+            Node(T val, unsigned char priority=0): value(std::move(val)), next(nullptr), priority(priority){}
             T value;
             Node* next;
+            unsigned char priority;
         };
         Node* first;
         Node* last;
@@ -18,8 +21,9 @@ class Queue {
     public:
         Queue();
         void enqueue(const T& val);
+        void enqueue(const T& val, unsigned char priority);
         T dequeue();
-        T peek();
+        T& peek();
         bool isEmpty();
         size_t count();
 };
@@ -35,25 +39,58 @@ void Queue<T>::enqueue(const T& val) {
         last = first;
         return;
     }
-    Node* tmp = last;
-    last = new Node(val);
-    tmp->next = last;
+    last->next = new Node(val);
+    last = last->next;
+}
+
+template<typename T>
+void Queue<T>::enqueue(const T &val, const unsigned char priority) {
+    if (isEmpty()) {
+        first = new Node(val, priority);
+        last = first;
+        return;
+    }
+    Node* ptr = first;
+    if (ptr->priority < priority){
+        first = new Node(val, priority);
+        first->next = ptr;
+        return;
+    }
+    while (ptr->next != nullptr){
+        if (ptr->next->priority < priority)
+            break;
+        ptr = ptr->next;
+    }
+    if (ptr->next == nullptr)
+        ptr->next = new Node(val, priority);
+    else {
+        Node* tmp = ptr->next;
+        ptr->next = new Node(val, priority);
+        ptr->next->next = tmp;
+    }
 }
 
 template<typename T>
 T Queue<T>::dequeue() {
     if (!isEmpty()){
-        Node* tmp = first->next;
         T ret = first->value;
-        delete first;
-        first = tmp;
-        return ret;
+        if (first == last){
+            delete first;
+            first = nullptr;
+            last = nullptr;
+            return ret;
+        } else {
+            Node* tmp = first->next;
+            delete first;
+            first = tmp;
+            return ret;
+        }
     }
     return T();
 }
 
 template<typename T>
-T Queue<T>::peek() {
+T& Queue<T>::peek() {
     return first->value;
 }
 
@@ -78,17 +115,21 @@ size_t Queue<T>::count() {
 
 int main() {
     Queue<int> q;
-    q.enqueue(3);
+    q.enqueue(3,9);
     q.enqueue(62);
     q.enqueue(903);
     q.enqueue(2);
+    q.enqueue(12345, 10);
+    q.enqueue(54321, 10);
+    std::cout << "peek: " << q.peek() << "\n";
     std::cout << "count: " << q.count() << "\n";
     std::cout << q.dequeue() << "\n";
     std::cout << q.dequeue() << "\n";
     std::cout << "count: " << q.count() << "\n";
     std::cout << q.dequeue() << "\n";
     std::cout << q.dequeue() << "\n";
-    std::cout << "might segfault\n";
+    std::cout << q.dequeue() << "\n";
+    q.enqueue(333, 2);
     std::cout << q.dequeue() << "\n";
     Queue<std::string> q2;
     q2.enqueue("XDD");
