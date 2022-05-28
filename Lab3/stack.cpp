98db85b796ba5b0a67351bb807cae71077a8035e
Bletinor <yjtv@protonmail.com>
2022-05-27 20:35:14 -0400

    added Node struct and push method

--- Lab3/stack.cpp
+++ Lab3/stack.cpp
@@ -1,3 +1,31 @@
-//
-// Created by main on 5/23/22.
-//
+//ENUNCIADO: Hacer programa que use stacks
+//PARTICIPANTES: 1101331 Asier Ochoa, 1099399 Yeuris Terrero, 1101629 Carlos Garcia, 1100685 Miguel Angel
+//FECHA: 27/5/22
+
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+void pushNode(Node *&stack, int num)
+{
+    Node *new_Node = new Node;
+    new_Node -> data = num;
+    new_Node -> next = stack;
+    stack = new_Node;
+}
+
+void popNode(Node *&stack, int num)
+{
+    Node *temp = stack;
+    num = temp -> data;
+}
+
+int main()
+{
+    Node* stack = nullptr;
+    pushNode(stack, 1);
+}
