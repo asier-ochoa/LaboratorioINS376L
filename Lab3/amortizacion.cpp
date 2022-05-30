7f3ce7f098cfd36d5879aee53c5f5194854f508d
asier-ochoa <asier-ochoa>
2022-05-30 00:44:19 -0400

    Tabla amortizacion.cpp terminado

--- Lab3/amortizacion.cpp
+++ Lab3/amortizacion.cpp
@@ -11,32 +11,66 @@
 class TablaAmortizacion{
     private:
         float* payments;
+        float* amountLeft;
+        float avg = 0;
         int len = 0;
     public:
         void GenerateTable(int months, float monthly_rate, float amount);
         void DeleteTable();
         static float GetQuota(float p, float i, int n);
+        void Draw();
 };
 
 void TablaAmortizacion::GenerateTable(int months, float monthly_rate, float amount) {
-    len = months;
-    payments = new float[months];
-    for (int i = 0; i < months; ++i) {
-        payments[i] = GetQuota(amount, monthly_rate, months - i);
-        std::cout << "Amount in month " << i << ": " << payments[i] << '\n';
-        amount -= payments[i];
+    if (len == 0) {
+        len = months;
+        payments = new float[months];
+        amountLeft = new float[months];
+        for (int i = 0; i < months; ++i) {
+            payments[i] = GetQuota(amount, monthly_rate, months - i);
+            avg += payments[i];
+            amount -= payments[i];
+            amountLeft[i] = (amount < 0 ? 0 : amount);
+        }
+        avg /= months;
     }
 }
 
 void TablaAmortizacion::DeleteTable() {
     if (len > 0){
         delete[] payments;
+        delete[] amountLeft;
         len = 0;
+        avg = 0;
     }
 }
 
 float TablaAmortizacion::GetQuota(float p, float i, int n) {
     return p*((i*std::pow(1+i, n))/(std::pow(1+i,n)-1));
+}
+
+void TablaAmortizacion::Draw() {
+    if (len > 0){
+        static ImGuiTableFlags f = ImGuiTableFlags_ScrollY | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV;
+        ImGui::Indent(50);
+        ImGui::BeginTable("Tabla de Amortizacion", 3, f, ImVec2(690, 450));
+        ImGui::TableSetupScrollFreeze(0, 1);
+        ImGui::TableSetupColumn("Mes");
+        ImGui::TableSetupColumn("Pago Mensual");
+        ImGui::TableSetupColumn("Pago Restante");
+        ImGui::TableHeadersRow();
+        for (int i = 0; i < len; ++i) {
+            ImGui::TableNextColumn();
+            ImGui::Text("%i", i + 1);
+            ImGui::TableNextColumn();
+            ImGui::Text("%.2f $", payments[i]);
+            ImGui::TableNextColumn();
+            ImGui::Text("%.2f $", amountLeft[i]);
+            ImGui::TableNextRow();
+        }
+        ImGui::EndTable();
+        ImGui::Text("Promedio de pagos mensuales: %.2f $", avg);
+    }
 }
 
 int main(){
@@ -50,7 +84,7 @@ int main(){
         BeginDrawing();
         ClearBackground(BLACK);
         rlImGuiBegin();
-        //ImGui::ShowDemoWindow();
+//        ImGui::ShowDemoWindow();
         ImGui::SetNextWindowSize(ImVec2(800,600));
         ImGui::SetNextWindowPos(ImVec2(0,0));
         if (ImGui::Begin("Main", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar)){
@@ -59,16 +93,19 @@ int main(){
             ImGui::PushItemWidth(width/3 - 80);
             ImGui::InputInt("Meses", &months); ImGui::SameLine();
             if (months < 1) months = 1;
-            ImGui::InputFloat("Monto",&amount); ImGui::SameLine();
-            ImGui::InputFloat("Tasa Anual", &tasaAnual);
-            tasaMensual = tasaAnual/12;
+            ImGui::InputFloat("Monto",&amount, 0, 0, "%.2f $"); ImGui::SameLine();
+            if (amount < 1) amount = 1;
+            ImGui::SliderFloat("Tasa Anual", &tasaAnual, 0, 100, "%.0f %%");
+            tasaMensual = (tasaAnual/100)/12;
             ImGui::PopItemWidth();
             static float xd;
             if (ImGui::Button("Generar Tabla", ImVec2(width/2, 40)))
                 t.GenerateTable(months, tasaMensual, amount);
             ImGui::SameLine();
-            ImGui::Button("Borrar Tabla", ImVec2(width/2, 40));
+            if (ImGui::Button("Borrar Tabla", ImVec2(width/2, 40)))
+                t.DeleteTable();
             ImGui::Separator();
+            t.Draw();
             ImGui::End();
         }
         rlImGuiEnd();
