7ffab42e718f8f17ec0cf0d39eba7f6451e20ced
asier-ochoa <asier-ochoa>
2022-06-05 12:47:46 -0400

    Arbitrary removal added

--- Lab4/linkedList.cpp
+++ Lab4/linkedList.cpp
@@ -23,11 +23,12 @@ class LinkedList {
     public:
         LinkedList();
         void Insert(T value);
-        T Remove();
+        T Remove(T value); //Remove from arbitrary place
+        T Dequeue(); //Remove from front
 
         //Functions for getting references to the values in the nodes
-        T& End();
-        T& operator[](T value); //Use this to access an element instead of peek
+        const T& End();
+        const T& operator[](T value); //Use this to access an element instead of peek
 };
 
 template<typename T>
@@ -64,7 +65,43 @@ void LinkedList<T>::Insert(T value) {
 }
 
 template<typename T>
-T &LinkedList<T>::operator[](T value) {
+T LinkedList<T>::Remove(T value) {
+    if (IsEmpty())
+        throw value_not_found();
+    Node* j = nullptr;
+    for (Node* i = first; i != nullptr; i = i->next) {
+        if (i->value == value){
+            Node* tmp = i->next;
+            T ret = i->value;
+            if (i == first){
+                first = tmp;
+                if (i == last)
+                    last = tmp;
+                delete i;
+                return ret;
+            }
+            if (i == last){
+                delete i;
+                j->next = nullptr;
+                last = j;
+                return ret;
+            }
+            delete i;
+            j->next = tmp;
+            return ret;
+        }
+        j = i;
+    }
+    throw value_not_found();
+}
+
+template<typename T>
+T LinkedList<T>::Dequeue() {
+    return nullptr;
+}
+
+template<typename T>
+const T &LinkedList<T>::operator[](T value) {
     if (IsEmpty()) //safety check
         throw value_not_found();
     for (Node* i = first; i != nullptr; i = i->next)
@@ -76,9 +113,10 @@ T &LinkedList<T>::operator[](T value) {
 int main(){
     LinkedList<int> l;
     l.Insert(3);
+    std::cout << l[3] << '\n';
+    l.Remove(3);
     l.Insert(4);
     l.Insert(4);
     l.Insert(2);
-    std::cout << l[3] << '\n';
     return 0;
 }
