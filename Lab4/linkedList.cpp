082fd21fee093d2b140f99a5ec028bc473f49556
asier-ochoa <asier-ochoa>
2022-06-04 23:54:53 -0400

    Added insertion n stuff. Might remove exceptions

--- Lab4/linkedList.cpp
+++ Lab4/linkedList.cpp
@@ -2,11 +2,17 @@
 //PARTICIPANTES: ID NOMBRE APELLIDO
 //FECHA:
 
+#include <string>
+#include <iostream>
+#include <exception>
+
+class value_not_found : public std::exception{};
+
 template <typename T>
 class LinkedList {
     private:
         struct Node{
-            Node();
+            explicit Node(T& val): value(val), next(nullptr){}
             T value;
             Node* next;
         };
@@ -16,15 +22,12 @@ class LinkedList {
 
     public:
         LinkedList();
-        void Push(T& value);
-        T Pop();
-
-        void Enqueue(T& value);
-        T Dequeue();
+        void Insert(T value);
+        T Remove();
 
         //Functions for getting references to the values in the nodes
         T& End();
-        T& operator[](T& value); //Use this to access an element instead of peek
+        T& operator[](T value); //Use this to access an element instead of peek
 };
 
 template<typename T>
@@ -32,19 +35,50 @@ LinkedList<T>::LinkedList()
     :first(nullptr), last(nullptr){}
 
 template<typename T>
-T &LinkedList<T>::operator[](T &value) {
-    if (IsEmpty()) //safety check
-        return nullptr;
-    Node* ptr = first;
-    while(ptr != nullptr) {
-        if (ptr->value == value){
-            return value;
+void LinkedList<T>::Insert(T value) {
+    if (IsEmpty()) { //If is empty, set both to the same node
+        first = new Node(value);
+        last = first;
+        return;
+    }
+    Node* newN = new Node(value);
+    for (Node* i = first; i != nullptr ; i = i->next) {
+        if (value <= i->value) {
+            if (i == first){ //In case it's inserted at head
+                newN->next = first;
+                first = newN;
+                return;
+            }
+            if (i == last){
+                last->next = newN;
+                last = newN;
+                return;
+            }
+            newN->next = i->next->next;
+            i->next = newN;
+            return;
         }
     }
-    return nullptr;
+    last->next = newN;
+    last = newN;
+}
+
+template<typename T>
+T &LinkedList<T>::operator[](T value) {
+    if (IsEmpty()) //safety check
+        throw value_not_found();
+    for (Node* i = first; i != nullptr; i = i->next)
+        if (i->value == value)
+            return i->value;
+    throw value_not_found();
 }
 
 int main(){
-    int a;
+    LinkedList<int> l;
+    l.Insert(3);
+    l.Insert(4);
+    l.Insert(4);
+    l.Insert(2);
+    std::cout << l[3] << '\n';
     return 0;
 }
