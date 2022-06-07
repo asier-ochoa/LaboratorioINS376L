8df98dd197f806ae831cf3da64fc6a628f6aa153
Bletinor <yjtv@protonmail.com>
2022-06-07 16:20:58 -0400

    added lab5 folder

---
+++ Lab5/elHora.cpp
@@ -0,0 +1,3 @@
+//ENUNCIADO: Buscar el hora del día del problema
+//PARTICIPANTES: 1101331 Asier Ochoa ; Yeuris Terrero | 1099399 ; Carlos Garcia | 1101629 ; Miguel Angel | 1100685
+//FECHA: 
---
+++ Lab5/persistNum.cpp
@@ -0,0 +1,3 @@
+//ENUNCIADO: Buscar el único número entero de solo dos dígitos cuyo número persistente es mayor que 3
+//PARTICIPANTES: 1101331 Asier Ochoa ; Yeuris Terrero | 1099399 ; Carlos Garcia | 1101629 ; Miguel Angel | 1100685
+//FECHA:
