7265a35ad0993a6f5816e2f3452f6a7641a17f71
Bletinor <yjtv@protonmail.com>
2022-06-12 23:45:03 -0400

    added algorithms to solve problems

--- Lab5/elHora.cpp
+++ Lab5/elHora.cpp
@@ -1,3 +1,51 @@
 //ENUNCIADO: Buscar el hora del día del problema
 //PARTICIPANTES: 1101331 Asier Ochoa ; Yeuris Terrero | 1099399 ; Carlos Garcia | 1101629 ; Miguel Angel | 1100685
-//FECHA: 
+//FECHA:
+
+/*La profesora Maria Jesus Budria explica a sus alumnos, en clase de matemáticas, 
+las intrigantes propiedades de los números enteros. Les mostró como el numero 
+8,833 es igual a 88^2 + 33^2 y prosiguió su clase. 
+
+Como es habitual en los alumnos, el avispado Michelena, no prestaba atención a 
+las explicaciones de su profesora observando, en su lugar, el reloj digital de la 
+pared. Así, advirtió que cuando la Srta. Budria acababa de hablar del numero 
+8,833, la hora que marcaba el reloj (tomada como un numero sin coma) tenia la 
+misma propiedad. Esto es, el cuadrado de los dígitos que indicaban la hora, mas 
+el cuadrado de los que indicaban los minutos, era igual al numero que veía en el 
+reloj.
+
+Cuál hora del dia era?*/
+
+#include <iostream>
+
+int getSquareSum(int hour, int minute)
+{
+    int x = hour * hour;
+    int y = minute * minute;
+
+    return x + y;
+
+}
+
+int main()
+{
+    for (size_t i = 1; i < 24; i++)
+    {
+        for (size_t j = 1; j < 60; j++)
+        {
+            std::string hour = std::to_string(i);
+            std::string minute = std::to_string(j);
+            std::string time = hour + minute;
+            int timeInt = std::stoi(time);
+            
+            int time2 = getSquareSum(i, j);
+
+            if (time2 == timeInt)
+            {
+                std::cout<< hour << ":" << minute << " Was the time of the day\n";
+            }
+        }
+        
+    }
+    
+}
--- Lab5/persistNum.cpp
+++ Lab5/persistNum.cpp
@@ -1,3 +1,57 @@
 //ENUNCIADO: Buscar el único número entero de solo dos dígitos cuyo número persistente es mayor que 3
 //PARTICIPANTES: 1101331 Asier Ochoa ; Yeuris Terrero | 1099399 ; Carlos Garcia | 1101629 ; Miguel Angel | 1100685
 //FECHA:
+
+/*Multiplicando los números de un número entero y continuando 
+el proceso, nos encontramos con el sorprendente resultado de 
+que la secuencia de productos llega siempre a un numero de un 
+solo digito. Por ejemplo 
+
+715 >  35 >  15 > 5 
+88 > 64 > 24 > 8 
+27 > 14 > 4
+
+Al numero de productos necesarios para alcazar un solo dígito 
+se le llama persistencia de ese numero entero. Es decir, 715 y 88 
+tienen persistencia 3, mientras que 27 tiene persistencia 2. 
+Cuál es el único numero entero de solo dos dígitos cuyo numero 
+persistente es mayor que 3?*/
+
+#include <iostream>
+
+int getMultDigits(int num)
+{
+	int digit = 0, digit2 = 0, result = 10, prst = 0;
+
+	while (result > 9)
+	{
+		digit = num % 10;
+		//std::cout << digit << " * ";
+		num /= 10;
+		//std::cout << num << " = ";
+		result = digit * num;
+		//std::cout << result << "\n";
+		num = result;
+		prst++;
+	}
+
+
+	return prst;
+}
+
+int main()
+{
+    for (size_t i = 10; i < 99; i++)
+	{
+		//std::cout << i << ":"<< "\n";
+		//std::cout << getMultDigits(i) << "\n";
+
+		int prst = getMultDigits(i);
+
+		if (prst > 3)
+		{
+			std::cout << i << "\n";
+		}
+		
+	}
+}
