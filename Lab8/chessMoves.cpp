98cd1b6d2562be50545f80ea790d36cc452dfd46
Bletinor <yjtv@protonmail.com>
2022-06-29 19:07:51 -0400

    added last lab files

---
+++ Lab8/chessMoves.cpp
@@ -0,0 +1,3 @@
+//ENUNCIADO: Dadas 2 torres enemigas y una reina, mostrar cuales casillas son movimientos válidos para la reina
+//PARTICIPANTES: 1101331 Asier Ochoa; Yeuris Terrero | 1099399; Carlos Garcia | 1101629; Miguel Angel | 1100685
+//FECHA: 4/07/22
