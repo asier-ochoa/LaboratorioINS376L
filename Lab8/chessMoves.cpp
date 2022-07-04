c9e0dc6d1f383eaad6f713d95046982ec48d970a
smugtwingo <smugtwingo>
2022-07-03 23:40:08 -0400

    Drawing possible moves done

--- Lab8/chessMoves.cpp
+++ Lab8/chessMoves.cpp
@@ -8,6 +8,8 @@
 #include "raymath.h"
 
 bool reDraw = true;
+Texture queenSprite;
+Texture towerSprite;
 
 //Coordinates origin is at top left. +Y is down and +X is right.
 class Piece {
@@ -15,6 +17,7 @@ class Piece {
         Piece(int posX, int posY, std::vector<Piece*>& activePieces): posX(posX), posY(posY), activePieces(activePieces) {}
         virtual void draw() = 0;
         virtual bool canMoveTo(int x, int y) = 0;
+        int team;
     protected:
         std::vector<Piece*>& activePieces;
         int posX, posY;
@@ -22,16 +25,16 @@ class Piece {
 
 class Queen : public Piece {
     public:
-        Queen(int posX, int posY, std::vector<Piece*>& board): Piece(posX, posY, board) {};
+        Queen(int posX, int posY, std::vector<Piece*>& board): Piece(posX, posY, board) {team = 0;};
         bool canMoveTo(int x, int y) override;
-        void draw() override {}
+        void draw() override { DrawTexture(queenSprite, posX * 100, posY * 100, WHITE);}
 };
 
 class Tower : public Piece {
     public:
-        Tower(int posX, int posY, std::vector<Piece*>& board): Piece(posX, posY, board) {};
+        Tower(int posX, int posY, std::vector<Piece*>& board): Piece(posX, posY, board) {team = 1;};
         bool canMoveTo(int x, int y) override;
-        void draw() override {}
+        void draw() override { DrawTexture(towerSprite, posX * 100, posY * 100, WHITE);}
 };
 
 bool Queen::canMoveTo(int x, int y) {
@@ -77,12 +80,14 @@ bool Tower::canMoveTo(int x, int y) {
 int main() {
     InitWindow(800, 950, "Chess Move Sim");
     SetTargetFPS(60);
-    LoadTextureFromImage(LoadImage("queen.png"));
-    LoadTextureFromImage(LoadImage("tower.png"));
+    queenSprite = LoadTextureFromImage(LoadImage("queen.png"));
+    towerSprite = LoadTextureFromImage(LoadImage("tower.png"));
     RenderTexture framebuffer = LoadRenderTexture(800, 800);
     std::vector<Piece*> pieces;
-    pieces.emplace_back(new Queen(3, 6, pieces));
+    pieces.emplace_back(new Queen(4, 6, pieces));
+    Piece& q_ref = *pieces.back();
     pieces.emplace_back(new Tower(1, 1, pieces));
+    pieces.emplace_back(new Tower(3, 1, pieces));
 
     while (!WindowShouldClose()){
         ClearBackground(BLACK);
@@ -94,15 +99,36 @@ int main() {
                 for (int j = 0; j < 8; ++j) {
                     Rectangle a = {(float)i * 100, (float)j * 100, 100, 100};
                     if (j % 2 == 0)
-                        DrawRectangleRec(a, (i % 2 == 0 ? Color{249, 246, 187, 255} : Color{127, 76, 31, 255}));
+                        DrawRectangleRec(a, (i % 2 == 0 ? Color{227, 191, 159, 255} : Color{82, 55, 33, 255}));
                     else
-                        DrawRectangleRec(a, (i % 2 != 0 ? Color{249, 246, 187, 255} : Color{127, 76, 31, 255}));
+                        DrawRectangleRec(a, (i % 2 != 0 ? Color{227, 191, 159, 255} : Color{82, 55, 33, 255}));
 
                 }
             }
             //Draw pieces
             for (int i = 0; i < pieces.size(); ++i) {
-
+                pieces[i]->draw();
+            }
+            //Draw possible moves
+            for (int i = 0; i < 8; ++i) {
+                for (int j = 0; j < 8; ++j) {
+                    if (q_ref.canMoveTo(i, j)){
+                        bool blocked = false;
+                        Rectangle a = {(float)i * 100, (float)j * 100, 100, 100};
+                        for (int k = 0; k < pieces.size(); ++k) {
+                            if (pieces[k]->team != 0){
+                                if (pieces[k]->canMoveTo(i, j)){
+                                    DrawRectangleRec(a, Color{255, 0, 0, 127});
+                                    blocked = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (!blocked)
+                            DrawRectangleRec(a, Color{0, 255, 0, 127});
+                    }
+                }
+                std::cout << '\n';
             }
             EndTextureMode();
         }
@@ -112,16 +138,7 @@ int main() {
         reDraw = false;
     }
 
-    Piece& q_ref = *pieces[0];
     Piece& t_ref = *pieces[1];
-
-    std::cout << "Queen:\n";
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            std::cout << q_ref.canMoveTo(i, j);
-        }
-        std::cout << '\n';
-    }
 
     std::cout << "Tower:\n";
     for (int i = 0; i < 8; ++i) {
